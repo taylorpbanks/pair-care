9c866ecbaf59e160b34c0c03fc1706d885d1cf11
Paircare <70249196+Paircare@users.noreply.github.com>
2021-12-21 17:37:28 -0500

    Update blog.jsx

--- src/constants/blog.jsx
+++ src/constants/blog.jsx
@@ -3,6 +3,8 @@ import blog2 from '../img/blog/maternity-bag.jpg';
 import blog3 from '../img/blog/baby-play.jpg';
 import blog4 from '../img/blog/first-baby.jpg';
 import blog6 from '../img/blog/laura-ohlman-xFlVj0HFNCs-unsplash.jpg';
+import blog7 from '../img/blog/harry-grout-ivfmiQ5FbHw-unsplash.jpg';
+
 
 export default {
   featured: {
@@ -132,4 +134,32 @@ export default {
 </ul>`
     },
   ]
-}
+},
+  {
+      id: 7,
+      title: 'Baby food: To cook or not to cook?!',
+      img: blog7,
+      category: 'essentials',
+      time: '2 minutes',
+      date: 'December 8, 2021',
+      html: '<p>The world is full of choices, and yes opinions, on what your child should be eating. You can grab an assortment of pre-made baby food and snacks from any big box store aisle such as Walmart and Target and of course there is Amazon. You can also go completely au natural and make your own delectable assortment of foods. But let&rsquo;s not forget the middle ground. You can combine pre-made food with food you&rsquo;ve made or even outsource the work of pre-made fresh food. The question comes down to three things how much time do you want to spend, how much money do you want to spend and how much control do you want over the nutritional value. Here are some of our favorites for each type of food path.</p>
+<p><u>For the parents who want to cut down on time: </u></p>
+<p>If you haven&rsquo;t heard there were reports of toxic heavy metals in baby foods. Some folks even took it to social media to demonstrate the simple act of using a magnet and pulling small fragments of metal from their purchase kid foods. Now there are class action lawsuits against Plum (a Campbell brand), Gerber, Beech-Nut and Hain. Fear not, US lawmakers are introducing The Baby Food Safety Act of 2021 that will limit toxic heavy metals in baby food which includes mercury, lead, inorganic arsenic and cadmium. The good news is there are a plethora of brands outside of the four listed in which you can stock up on for your little one.</p>
+<p>We love the variety from the happy family of products. From teether crackers to food pouches to their warmup meals. You&rsquo;ll feel great about the options, and they do a great job of incorporating healthy foods such as quinoa, chia, and a variety of vegetables.</p>
+<p> </p>
+<p><u>For the parents who prefers to be their own chef: </u></p>
+<p>You&rsquo;ll need something to steam your food, blend it all up, and store. You can do as simple as getting a steamer basket, using a blender you may already have (Immersion Blender, Nutribullet, Vitamix, etc.), and store in Tupperware that you already have. If you want an upgrade on some times that help combines some steps.</p>
+<p><a href="https://www.walmart.com/ip/Baby-Food-Maker-Baby-Food-Processor-Blender-Grinder-Steamer-Cooks-Blends-Healthy-Homemade-Baby-Food-in-Minutes/382301533?wmlspartner=wlpa&amp;selectedSellerId=101066175&amp;&amp;adid=22222222227463468544&amp;wl0=&amp;wl1=g&amp;wl2=c&amp;wl3=540339639335&amp;wl4=pla-1410366983475&amp;wl5=1022762&amp;wl6=&amp;wl7=&amp;wl8=&amp;wl9=pla&amp;wl10=377866564&amp;wl11=online&amp;wl12=382301533&amp;veh=sem&amp;gclid=Cj0KCQjwg7KJBhDyARIsAHrAXaEaDEWevoLiQhiss7gmt8FXjtPpg98W-QZp748FS_jDFE79yq0mtOAaAubVEALw_wcB&amp;gclsrc=aw.ds">MaxKare three-in-one food processor</a></p>
+<p>Baby Food Maker Baby Food Processor Blender Grinder Steamer Cooks &amp; Blends Healthy Homemade Baby Food in Minutes</p>
+<h1><a href="https://www.target.com/p/haakaa-baby-feeding-spoon-gray/-/A-80261722?ref=tgt_adv_XS000000&amp;AFID=google_pla_df&amp;fndsrc=tgtao&amp;DFA=71700000012510679&amp;CPNG=PLA_Baby%2BShopping%7CBaby_Ecomm_Baby&amp;adgroup=SC_Baby_High%2BMargin&amp;LID=700000001170770pgs&amp;LNM=PRODUCT_GROUP&amp;network=g&amp;device=c&amp;location=1022762&amp;targetid=pla-923670360630&amp;ds_rl=1242884&amp;ds_rl=1246978&amp;ds_rl=1248099&amp;gclid=Cj0KCQjwg7KJBhDyARIsAHrAXaFijLVYtXHjuSAL21sv_zglX7UL2SoknlW8we57skwllQfX8e-A2RAaAgU5EALw_wcB&amp;gclsrc=aw.ds">Haakaa Baby Feeding Spoon</a>. Great for easy feeding and ready to go. Also great for heating as it is able to resist boiling temperatures.</h1>
+<h1><a href="https://www.target.com/p/infantino-fresh-squeezed-squeeze-pouches-50-pack/-/A-14326420?ref=tgt_adv_XS000000&amp;AFID=google_pla_df&amp;fndsrc=tgtao&amp;DFA=71700000012510679&amp;CPNG=PLA_Baby%2BShopping%7CBaby_Ecomm_Baby&amp;adgroup=SC_Baby_High%2BMargin&amp;LID=700000001170770pgs&amp;LNM=PRODUCT_GROUP&amp;network=g&amp;device=c&amp;location=1022762&amp;targetid=pla-923670360630&amp;ds_rl=1242884&amp;ds_rl=1246978&amp;ds_rl=1248099&amp;gclid=Cj0KCQjwg7KJBhDyARIsAHrAXaGju9v2eMdO9WOWzudAnizOvOEjJKabwQayDRWIPVgTHzfxAz3NxGkaAtjjEALw_wcB&amp;gclsrc=aw.ds">Infantino Fresh Squeezed - Squeeze Pouches (50 Pack)</a></h1>
+<p>These Infantino Fresh Squeezed Squeeze Pouches are a perfect way to store and serve homemade purees or baby food. With a slim, squeezable shape that's perfect for little hands and a convenient air-tight cap, they're great for storing your creations in the fridge or freezer and for taking them with you on the go. These pouches are to be used with the Infantino Squeeze Station. Holds up to 4oz. of puree. BPA, PVC, phthalate free.</p>
+<h1><a href="https://www.amazon.com/Squooshi-Reusable-Pouch-Animal-Pouches/dp/B01IYW6AVS/ref=asc_df_B01IYW6AVS/?tag=hyprod-20&amp;linkCode=df0&amp;hvadid=198075546174&amp;hvpos=&amp;hvnetw=g&amp;hvrand=2686000809819986678&amp;hvpone=&amp;hvptwo=&amp;hvqmt=&amp;hvdev=c&amp;hvdvcmdl=&amp;hvlocint=&amp;hvlocphy=1022762&amp;hvtargid=pla-358836069011&amp;psc=1">SQUOOSHI Reusable Baby Food Pouches 5 oz - 6 Large Pouches</a></h1>
+<p>MADE OF DURABLE MATERIAL: Our reusable snack bags are made to last with durable, BPA-free, Phthalate, PVC &amp; Lead-free materials with a no choke cap. These food pouches are completely leak-proof. Airtight double zip closure keeps food fresh and liquids contained. Little hands can&rsquo;t squeeze the pouch open.</p>
+<p>Lastly, need to be inspired with some recipes check out: <a href="https://www.yummytoddlerfood.com/">https://www.yummytoddlerfood.com/</a>.</p>
+<p><u>For the parents who wants to hire someone else to be the chef: </u></p>
+<p>While this market is only getting more saturated one brand that has stood out to us is Yumi. It&rsquo;s as easy as picking a plan, they prep, cook, and ship to you. Voila you have a week supply of food! And it&rsquo;s as simple as they ship fresh food EVERY week. Yumi has amazing customer service and have developed a food plan (7 stages) that adapts to your baby&rsquo;s likes while also allowing them to try new things. New things that even you may not have tried yourself such as adzuki bean, Japanese sweet potato, dragon fruit, and more. They also pride themselves on being no gluten, dairy, big 9 allergens, GMO, added sugars, or fortified so you have even less to worry about if you have specific dietary guidelines you wish to follow.</p>
+<p>https://helloyumi.com/</p>
+<p>At the end of the day, similar to one&rsquo;s birth plan, expect to be flexibility or you will drive yourself mad. Remember that your child has their own taste preferences. As a parent you can do your best to make sure they develop the best pallet, but it is always best when our little ones have full stomachs and happy hearts.</p>
+</div>'
+    },
